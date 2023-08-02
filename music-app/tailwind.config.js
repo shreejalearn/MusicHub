ea7f3240f4a7cad5861a95c2d375e95481019351
shreejalearn <shreeja.das.16@gmail.com>
2023-08-02 11:16:19 -0400

    more setup

--- music-app/tailwind.config.js
+++ music-app/tailwind.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  mode:'jit',
+  purge:["./src/**/*.{js,jsx,ts,tsx}","./public/index.html"],
+  darkMode:false,
   theme: {
     extend: {},
+  },
+  variants: {
+    extend:{},
   },
   plugins: [],
 }
