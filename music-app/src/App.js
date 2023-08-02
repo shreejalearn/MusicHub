8ea1418f7e2a2c47d3f987bb4953bbeca014d123
shreejalearn <shreeja.das.16@gmail.com>
2023-08-02 14:55:06 -0400

    homepage more edits

--- music-app/src/App.js
+++ music-app/src/App.js
@@ -2,16 +2,28 @@ import React from 'react';
 import headerImage from './assets/resonate-left-top.png'; // Import the image
 import logo_mic from './assets/logo_mic.png'; // Import the image
 import leaf_design from './assets/leafdesign.png'; // Import the image
+import text_sub from './assets/text-sub.png'; // Import the image
+import texture from './assets/texture.png'; // Import the image
 
 const App = () => {
   return (
-    <div className="bg-green-resonate min-h-screen flex flex-col items-center justify-center">
+    <div className="bg-green-resonate min-h-screen flex flex-col items-center justify-center relative">
       <img src={headerImage} alt="Music Hub" className="w-1/2 absolute top-0 left-0 ml-5 mt-7" />
-      <div className="flex items-center">
-        <h1 className="font-reborn text-9xl text-black-resonate mx-[-10px] mt-7">Res</h1>
-        <img src={logo_mic} alt="microphone" className="h-76 w-76 mt-28" /> {/* Adjust the margin */}
-        <img src={leaf_design} alt="Leaf Design" className="absolute h-24 w-24 top-56 right-80" /> {/* Adjust the margin */}
-        <h1 className="font-reborn text-9xl text-black-resonate mx-[-25px] mt-7">nate</h1>
+      
+      <div className="flex items-center relative mt-40"> {/* Increase the margin-top */}
+        <h1 className="font-reborn text-9xl text-black-resonate mr-[-25px]">Res</h1>
+        <img src={logo_mic} alt="microphone" className="h-152 w-152 ml-4 mt-20" />
+        <h1 className="font-reborn text-9xl text-black-resonate mx-[-25px]">nate</h1>
+        <img src={leaf_design} alt="Leaf Design" className="h-24 w-24 mt-[-29%]" />
+      </div>
+
+      <div className="flex justify-center mt-12"> {/* Increase the margin-top */}
+        <img src={text_sub} alt="Connect Create Collaborate" className="h-auto mt-0" />
+      </div>
+
+      {/* Overlay texture background */}
+      <div className="absolute top-20 left-0 w-full h-full bg-opacity-50">
+        <img src={texture} alt="textured background" className="w-full h-full object-cover" />
       </div>
     </div>
   );
