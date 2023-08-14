9bd08f2a71bd3de6cec89a9569984959d6d072fe
aekmekci72 <anna.ekmekci@gmail.com>
2023-08-13 23:42:31 -0400

    :)

--- server/controllers/dbController.js
+++
@@ -1,8 +0,0 @@
-const pool = require('./db/db_connection'); // Your database connection pool
-
-const saveFilePath = (filePath) => {
-  const query = 'INSERT INTO ccchallenge (file) VALUES ($1)';
-  return pool.query(query, [filePath]);
-};
-
-module.exports = { saveFilePath };
