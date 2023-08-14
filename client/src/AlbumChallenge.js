62be763a942abb3f88e79e330e459e83be3c2e26
shreejalearn <shreeja.das.16@gmail.com>
2023-08-14 15:39:53 -0400

    album challenge design updated!

--- client/src/AlbumChallenge.js
+++ client/src/AlbumChallenge.js
@@ -3,23 +3,28 @@ import FileUpload from './FileUpload';
 import sheetmusic from './assets/sheetmusic.png'; // Import the image
 import design_album from './assets/design_album.png';
 import side_design_album from './assets/side_design_album.png';
+import person from './assets/person.png';
+import person2 from './assets/person2.png';
 
 
 function AlbumChallenge() {
     return (
-        <div className="bg-yellow2-resonate min-h-screen flex flex-col items-center relative">
+        <div className="bg-yellow2-resonate min-h-screen flex flex-col items-center relative overflow-x-hidden">
             <div className="flex items-center flex-col"> {/* Center content vertically */}
-            <h1 className="font-reborn text-9xl text-[#979D92] mt-[8%]">Album</h1>
+            <img src={person} alt="person design" className="w-[20rem] ml-[-60%] mt-[10%] z-30" />
+            <h1 className="font-reborn text-9xl text-[#979D92] mt-[-18%]">Album</h1>
             <img src={sheetmusic} alt="sheet music design" className="" />
             <h1 className="font-reborn text-9xl text-[#979D92] z-20">Challenge</h1>
             <img src={design_album} alt="Album design" className="w-[140rem]" />
             </div>
             <img src={side_design_album} alt="Album design 2" className="mt-[5%] ml-[-65%]" />
-            <p className="CG_Reg max-w-[700px] overflow-hidden text-2xl text-black-resonate mr-[-30%] mt-[-37%]">1. "Ethereal Reverie" is a album that explores dreamlike landscapes of the human imagination. The album features a collection of  electronic tracks that transports listeners to a different realm. Create a design that reflects your imagination and thoughts about this theme. The final design should be submitted as a high-resolution PNG or JPG file. Consider the dimensions required for album cover artwork, typically around 3000 x 3000 pixels.<br /><br /><br />2. Free Form: "Midnight Mirage" is a new album with pop and soft country songs. Create a design that reflects your imagination and thoughts about this theme. The design should convey a sense of duality and harmony, reflecting the merging of pop and soft country music.
+            <p className="CG_Reg max-w-[700px] overflow-hidden text-2xl text-black-resonate mr-[-20%] mt-[-37%]">1. "Ethereal Reverie" is a album that explores dreamlike landscapes of the human imagination. The album features a collection of  electronic tracks that transports listeners to a different realm. Create a design that reflects your imagination and thoughts about this theme. The final design should be submitted as a high-resolution PNG or JPG file. Consider the dimensions required for album cover artwork, typically around 3000 x 3000 pixels.<br /><br /><br />2. Free Form: "Midnight Mirage" is a new album with pop and soft country songs. Create a design that reflects your imagination and thoughts about this theme. The design should convey a sense of duality and harmony, reflecting the merging of pop and soft country music.
             The final design should be submitted as a high-resolution PNG or JPG file. Consider the dimensions required for album cover artwork, typically around 3000 x 3000 pixels.
 </p>
+<img src={person2} alt="person 2 design" className="w-[25rem] mr-[-85%] mt-[-16%]" />
 
-            <h1 className="font-reborn text-9xl text-[#979D92] mt-[12%]">Submissions</h1>
+
+            <h1 className="font-reborn text-9xl text-[#979D92] mt-[1%]">Submissions</h1>
             <div className="mt-[5%] bg-[#D6C1C1] rounded-lg shadow-lg p-8 w-full max-w-md">
                 <h1 className="text-3xl font-semibold mb-6 text-white">Upload Video or Audio</h1>
                 <FileUpload />
