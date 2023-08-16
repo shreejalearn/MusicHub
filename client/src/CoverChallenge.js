3aa71f6558767d243c1d828144d23ae14286746c
aekmekci72 <anna.ekmekci@gmail.com>
2023-08-16 00:34:13 -0400

    files ugh

--- client/src/CoverChallenge.js
+++ client/src/CoverChallenge.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 // import FileUpload from './FileUpload';
 import neon from './assets/neon.png'; // Import the image
 import headphonesColor from './assets/headphonesColor.png'; // Import the image
@@ -13,23 +13,52 @@ import axios from 'axios';
 
 function CoverChallenge() {
   const [file, setFile] = useState(null);
+  const [showConfirmPopup, setShowConfirmPopup] = useState(false);
   const handleFileChange = (event) => {
-    setFile(event.target.files[0]);
+    const selectedFile = event.target.files[0];
+    setFile(selectedFile);
     console.log("file changed")
     console.log(event.target.files[0])
+    
   };
 
   const handleUpload = () => {
     if (file) {
-      const formData = new FormData();
-      formData.append('file', file);
-      console.log(formData)
-
-      axios.post('http://localhost:5000/ccupload', formData)
+      setShowConfirmPopup(true); // Show the confirm popup
     } else {
       console.error('No file selected');
     }
   };
+
+  const confirmUpload = () => {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    axios.post('http://localhost:5000/ccupload', formData)
+      .then(() => {
+        setFile(null); // Clear the uploaded file
+        setShowConfirmPopup(false); // Hide the confirm popup
+      })
+      .catch((error) => {
+        console.error('Error uploading file:', error);
+      });
+      
+  };
+
+  const cancelUpload = () => {
+    setFile(null);
+    setShowConfirmPopup(false); // Hide the confirm popup
+  };
+
+  useEffect(() => {
+    // This code will run whenever the 'file' state changes
+    if (file === null) {
+      const inputElement = document.getElementById('fileInput');
+      if (inputElement) {
+        inputElement.value = ''; // Clear the input value
+      }
+    }
+  }, [file]);
   
   return (
     <div className="bg-yellow2-resonate min-h-screen flex flex-col items-center relative">
@@ -54,13 +83,40 @@ function CoverChallenge() {
 
       <div className="mt-[5%] bg-[#D6C1C1] rounded-lg shadow-lg p-8 w-full max-w-md">
     <h1 className="text-3xl font-semibold mb-6 text-white">Upload Video or Audio</h1>
-    <input type="file" onChange={handleFileChange} />
-    <div className="mt-6">
-        <button onClick={handleUpload} className="bg-[#F1F1E7] hover:bg-[#F1EDD2] text-black-resonate font-semibold py-2 px-4 rounded">
-            Upload
-        </button>
-    </div>
-</div>
+    <input type="file" id="fileInput" onChange={handleFileChange} />
+        {}
+        <div className="mt-6">
+          {file ? (
+            <div>
+            <button
+            onClick={() => {
+              setFile(null);
+            }}
+            className="bg-[#F1F1E7] hover:bg-[#F1EDD2] text-black-resonate font-semibold py-2 px-4 rounded"
+          >Clear</button>
+            
+            <button
+            onClick={handleUpload}
+            className="bg-[#F1F1E7] hover:bg-[#F1EDD2] text-black-resonate font-semibold py-2 px-4 rounded"
+          >Upload</button>
+          </div>
+          ) : (
+            <p>Select a file to upload</p>
+          )}
+        </div>
+      </div>
+{/* Confirm/Cancel Popup */}
+{showConfirmPopup && (
+        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50">
+          <div className="bg-white p-8 rounded-lg shadow-lg">
+            <p className="text-xl mb-4">Are you sure you want to upload the file?</p>
+            <div className="flex justify-end">
+              <button onClick={confirmUpload} className="mr-4 text-blue-500 hover:text-blue-700">Confirm</button>
+              <button onClick={cancelUpload} className="text-red-500 hover:text-red-700">Cancel</button>
+            </div>
+          </div>
+        </div>
+      )}
 
       <p className="font-CG_Reg mb-[4%] max-w-[1300px] overflow-hidden text-2xl text-black-resonate mt-[5%]">Please note that submissions received after September 1st will be rejected. Any unsolicited submissions or emails will be deleted unread. The selection of winners is at the sole discretion of the judging panel. The prizes for this competition entail a mention on our website and a digital certificate. Submissions will be evaluated based on their adherence to either theme and their ability to craft a creative and powerful story in their video/audio clip. Each category will have 1 winner and possible honoroable mentions (who will get featured on our website)!
 </p>
--- client/src/LyricChallenge.js
+++ client/src/LyricChallenge.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import banner from './assets/banner.png'; // Import the image
 import guitar from './assets/guitar.png'; // Import the image
 import shakesphere from './assets/shakesphere.png'; // Import the image
@@ -8,23 +8,52 @@ import axios from 'axios';
 
 const LyricChallenge = () => {
   const [file, setFile] = useState(null);
+  const [showConfirmPopup, setShowConfirmPopup] = useState(false);
   const handleFileChange = (event) => {
-    setFile(event.target.files[0]);
+    const selectedFile = event.target.files[0];
+    setFile(selectedFile);
     console.log("file changed")
     console.log(event.target.files[0])
+    
   };
 
   const handleUpload = () => {
     if (file) {
-      const formData = new FormData();
-      formData.append('file', file);
-      console.log(formData)
-
-      axios.post('http://localhost:5000/lcupload', formData)
+      setShowConfirmPopup(true); // Show the confirm popup
     } else {
       console.error('No file selected');
     }
   };
+
+  const confirmUpload = () => {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    axios.post('http://localhost:5000/lcupload', formData)
+      .then(() => {
+        setFile(null); // Clear the uploaded file
+        setShowConfirmPopup(false); // Hide the confirm popup
+      })
+      .catch((error) => {
+        console.error('Error uploading file:', error);
+      });
+      
+  };
+
+  const cancelUpload = () => {
+    setFile(null);
+    setShowConfirmPopup(false); // Hide the confirm popup
+  };
+
+  useEffect(() => {
+    // This code will run whenever the 'file' state changes
+    if (file === null) {
+      const inputElement = document.getElementById('fileInput');
+      if (inputElement) {
+        inputElement.value = ''; // Clear the input value
+      }
+    }
+  }, [file]);
 
   return (
     <div className="bg-yellow2-resonate min-h-screen flex flex-col items-center relative">
@@ -44,12 +73,39 @@ const LyricChallenge = () => {
     <img src={retro} alt="retro design" className="w-[85rem] mt-[0%]" />
     
     <h1 className="text-3xl font-semibold mb-6 text-black">Upload PDF</h1>
-    <input type="file" onChange={handleFileChange} />
-    <div className="mt-6">
-        <button onClick={handleUpload} className="bg-[#F1F1E7] hover:bg-[#F1EDD2] text-black-resonate font-semibold py-2 px-4 rounded">
-            Upload
-        </button>
-    </div>
+    <input type="file" id="fileInput" onChange={handleFileChange} />
+        {}
+        <div className="mt-6">
+          {file ? (
+            <div>
+            <button
+            onClick={() => {
+              setFile(null);
+            }}
+            className="bg-[#F1F1E7] hover:bg-[#F1EDD2] text-black-resonate font-semibold py-2 px-4 rounded"
+          >Clear</button>
+            
+            <button
+            onClick={handleUpload}
+            className="bg-[#F1F1E7] hover:bg-[#F1EDD2] text-black-resonate font-semibold py-2 px-4 rounded"
+          >Upload</button>
+          </div>
+          ) : (
+            <p>Select a file to upload</p>
+          )}
+        </div>
+{/* Confirm/Cancel Popup */}
+{showConfirmPopup && (
+        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50">
+          <div className="bg-white p-8 rounded-lg shadow-lg">
+            <p className="text-xl mb-4">Are you sure you want to upload the file?</p>
+            <div className="flex justify-end">
+              <button onClick={confirmUpload} className="mr-4 text-blue-500 hover:text-blue-700">Confirm</button>
+              <button onClick={cancelUpload} className="text-red-500 hover:text-red-700">Cancel</button>
+            </div>
+          </div>
+        </div>
+      )}
 
 <p className="font-CG_Reg max-w-[1190px] mb-[4%] mt-[3%] overflow-hidden text-xl text-black-resonate">"Please note that submissions received after September 1st will be rejected. Any unsolicited submissions or emails will be deleted unread. The selection of winners is at the sole discretion of the judging panel. The prizes for this competition entail a mention on our website and a digital certificate. Submissions will be evaluated based on their adherence to either theme and their ability to craft a creative and engaging story within their lyrics. Each category will have 1 winner and possible honoroable mentions (who will get featured on our website)!"
 </p>
