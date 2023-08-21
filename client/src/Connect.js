027786c7c55d11af840677e9e4639c41d72e0583
shreejalearn <shreeja.das.16@gmail.com>
2023-08-21 14:07:54 -0400

    simple edits

--- client/src/Connect.js
+++ client/src/Connect.js
@@ -4,7 +4,7 @@ import connectdesign from './assets/connectdesign.png';
 
 const Connect = () => {
   return (
-    <div className="bg-yellow2-resonate min-h-screen flex flex-col items-center relative">
+    <div className="bg-green-resonate min-h-screen flex flex-col items-center relative">
       <div className="flex items-center flex-col mt-[14%]"> {/* Center content vertically */}
         <h1 className="font-reborn text-9xl text-[#979D92] z-20">Connect</h1>
         <img src={connectdesign} alt="connect design" className="w-[90rem] mt-[-8%]" />
