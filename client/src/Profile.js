1dbe21ad1c37fbc1483df08babb3722295435298
shreejalearn <shreeja.das.16@gmail.com>
2023-09-25 15:31:31 -0400

    profile frontend done!

--- client/src/Profile.js
+++ client/src/Profile.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import bg_profile2 from './assets/bg_profile2.png';
+import bgProfile3 from './assets/bgProfile3.png';
 import profileheading from './assets/profileheading.png';
 import profilePictures from './profilePictures/profilePictures'; // Import the profilePictures object
+import lineDesign from './assets/linedesign.png';
 
 const Profile = () => {
   const [userProfile, setUserProfile] = useState(null);
@@ -93,89 +94,84 @@ const Profile = () => {
   };
 
   return (
-    
-    <div className="bg-yellow-resonate min-h-screen flex flex-col items-center relative">
+    <div className="bg-yellow-resonate min-h-screen flex flex-col items-center relative" style={{ backgroundImage: `url(${bgProfile3})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
       <div className="flex items-center flex-col mt-[14%]">
-      <div>
-          
-          <div classname="mt-[-15%]">
-            <img src={bg_profile2} alt="Profile Background" className="w-full" />
-            <img src={profileheading} alt="Profile Heading" className="absolute top-[12%] left-[17%] right-0 bottom-0 flex items-center w-[62rem] z-20" />
-            <div>{userProfile && (
-              <img
-                src={profilePictures[userProfile.pfp]}
-                alt="Profile"
-                className="mt-4 rounded-full w-40 h-40 items-center"
-              />
-            )}
+        <div className='ml-[30%]'>
+          <div className="mt-[-18%] items-center ">
+            <img src={profileheading} alt="Profile Heading" className="flex items-center w-[300rem] mt-[-15%] ml-[-25%]" />
+            <img src={lineDesign} alt="Line Design" className="flex items-center w-[500rem] ml-[-20%] mt-[-40%]" />
+            <div className="mt-[-25%] ml-[7%]">
+              {userProfile && (
+                <img
+                  src={profilePictures[userProfile.pfp]}
+                  alt="Profile"
+                  className="rounded-full h-40 items-center ml-[12%]"
+                />
+              )}
             </div>
-        
-            
-            {userProfile && (
-  <div className='mt-[-15%]'>
-    {/* Display user information */}
-    {Object.keys(userProfile).map((field) => {
-      // Skip the 'pfp' field
-      if (field === 'pfp') {
-        return null; // Skip rendering 'pfp'
-      }
+          </div>
 
-      return (
-        <div
-          key={field}
-          className="flex items-center space-x-1 font-CG_Reg text-[#CD7417] overflow-x-hidden justify-center"
-          style={{ overflowWrap: 'break-word' }}
-        >
-          {isEditing && editingField === field ? (
-            <>
-              <input
-                type="text"
-                value={newValue}
-                onChange={(e) => setNewValue(e.target.value)}
-                className="border rounded px-2 py-1"
-              />
-              <button
-                onClick={handleSubmitEdit}
-                className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-1 px-2 rounded"
-              >
-                Submit
-              </button>
-            </>
-          ) : (
-            <>
-              <label
-                htmlFor={field}
-                className="text-[#679B89] font-CG_Reg text-3xl transition-colors hover:text-[#C2899E]"
-              >
-                {field.charAt(0).toUpperCase() + field.slice(1)}: {userProfile[field]}{' '}
-                <button
-                  onClick={() => handleEdit(field)}
-                  className="text-[#679B89] hover:text-[#C2899E] transition-colors"
-                >
-                  Edit
-                </button>
-              </label>
-            </>
+          {userProfile && (
+            <div className='mt-5 w-[400px] text-center ml-[7%]'>
+              {/* Display user information */}
+              {Object.keys(userProfile).map((field) => {
+                // Skip the 'pfp' field
+                if (field === 'pfp') {
+                  return null; // Skip rendering 'pfp'
+                }
+
+                return (
+                  <div
+                    key={field}
+                    className="flex items-center space-x-1 font-CG_Reg text-[#CD7417] overflow-x-hidden justify-center"
+                    style={{ overflowWrap: 'break-word' }}
+                  >
+                    {isEditing && editingField === field ? (
+                      <>
+                        <input
+                          type="text"
+                          value={newValue}
+                          onChange={(e) => setNewValue(e.target.value)}
+                          className="border rounded px-2 py-1"
+                        />
+                        <button
+                          onClick={handleSubmitEdit}
+                          className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-1 px-2 rounded"
+                        >
+                          Submit
+                        </button>
+                      </>
+                    ) : (
+                      <>
+                        <label
+                          htmlFor={field}
+                          className="text-[#679B89] font-CG_Reg text-3xl transition-colors hover:text-[#C2899E]"
+                        >
+                          {field.charAt(0).toUpperCase() + field.slice(1)}: {userProfile[field]}{' '}
+                          <button
+                            onClick={() => handleEdit(field)}
+                            className="text-[#679B89] hover:text-[#C2899E] transition-colors"
+                          >
+                            Edit
+                          </button>
+                        </label>
+                      </>
+                    )}
+                  </div>
+                );
+              })}
+            </div>
           )}
-        </div>
-      );
-    })}
-  </div>
-)}
 
-
-            
-
-            <div className="justify-center items-center space-x-1 mt-[5%] bg-[#D6C1C1] rounded-lg shadow-lg p-8 w-full max-w-md">
-              <h1 className="text-3xl font-semibold mb-6 text-white">Upload Profile Picture</h1>
-              <input type="file" id="fileInput" onChange={handleFileChange} />
-              <button
-                onClick={handleUpload}
-                className="bg-[#F1F1E7] hover:bg-[#F1EDD2] text-black-resonate font-semibold py-2 px-4 rounded"
-              >
-                Upload
-              </button>
-            </div>
+          <div className="justify-center items-center space-x-1 mt-5 bg-[#D6C1C1] rounded-lg shadow-lg p-8 w-full max-w-md ml-[5%] mb-[10%]">
+            <h1 className="text-3xl font-semibold mb-6 text-white">Upload Profile Picture</h1>
+            <input type="file" id="fileInput" onChange={handleFileChange} />
+            <button
+              onClick={handleUpload}
+              className="bg-[#F1F1E7] hover:bg-[#F1EDD2] text-black-resonate font-semibold py-2 px-4 rounded"
+            >
+              Upload
+            </button>
           </div>
         </div>
       </div>
