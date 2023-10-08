be3ba72e13905e814351ee88410f2653991da05a
shreejalearn <shreeja.das.16@gmail.com>
2023-10-07 20:17:59 -0400

    frontend friends

--- client/src/Friends.js
+++ client/src/Friends.js
@@ -1,7 +1,6 @@
-// Friends.js
-
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
+import design from './assets/design.png'; // Import the image
 
 const Friends = () => {
   const [friends, setFriends] = useState([]);
@@ -26,17 +25,17 @@ const Friends = () => {
 
     // Fetch friend requests using a POST request to your API endpoint
     const fetchFriendRequests = async () => {
-        try {
-            const loggedInUsername = localStorage.getItem('username');
-            if (loggedInUsername) {
-              const response = await Axios.post('http://localhost:5000/getfriendrequests', {
-                username: loggedInUsername,
-              });
-              setFriendRequests(response.data.requests);
-            }
-          } catch (error) {
-            console.error('Error fetching friend requests:', error);
-          }
+      try {
+        const loggedInUsername = localStorage.getItem('username');
+        if (loggedInUsername) {
+          const response = await Axios.post('http://localhost:5000/getfriendrequests', {
+            username: loggedInUsername,
+          });
+          setFriendRequests(response.data.requests);
+        }
+      } catch (error) {
+        console.error('Error fetching friend requests:', error);
+      }
     };
 
     fetchFriends();
@@ -52,10 +51,10 @@ const Friends = () => {
           username: loggedInUsername,
           friendUsername: request.username,
         });
-  
+
         // Remove the accepted request from the friendRequests state
         setFriendRequests((prevRequests) => prevRequests.filter((req) => req.username !== request.username));
-  
+
         // Add the accepted friend to the friends list
         setFriends((prevFriends) => [...prevFriends, request]); // Assuming 'request' contains friend data
       }
@@ -63,7 +62,6 @@ const Friends = () => {
       console.error('Error accepting friend request:', error);
     }
   };
-  
 
   const handleDeclineRequest = async (request) => {
     // Implement the logic to decline a friend request
@@ -83,32 +81,47 @@ const Friends = () => {
   };
 
   return (
-    <div className="friends-container">
-      <h1>My Friends</h1>
-      <ul>
-        {friends.map((friend) => (
-          <li key={friend.username}>
-            <p>Name: {friend.name}</p>
-            <p>Email: {friend.email}</p>
-            <p>Phone: {friend.phone}</p>
-          </li>
-        ))}
-      </ul>
+    <div className="flex justify-center items-center pt-[5%] bg-[#fafafa]" style={{ backgroundImage: `url(${design})`, backgroundSize: 'cover', backgroundPosition: 'center', opacity: 0.7 }}>
 
-      <h2>Friend Requests</h2>
-      <ul>
-        {friendRequests.map((request) => (
-          <li key={request.username}>
-            <p>Name: {request.name}</p>
-            <p>Main Instrument: {request.main_instrument}</p>
-            <p>Bio: {request.bio}</p>
-            <p>Skills: {request.skills}</p>
-            <p>Accolades: {request.accolades}</p>
-            <button onClick={() => handleAcceptRequest(request)}>Accept</button>
-            <button onClick={() => handleDeclineRequest(request)}>Decline</button>
-          </li>
-        ))}
-      </ul>
+      <div className="bg-[#F4F4F1] shadow-md p-8 rounded-lg w-[80%] z-20">
+        <h1 className="text-2xl text-[#979D92] font-semibold mb-6 font-reborn">My Friends</h1>
+        <ul>
+          {friends.map((friend) => (
+            <li key={friend.username} className="mb-6">
+              <p className="text-lg font-medium font-CG_Reg">Name: {friend.name}</p>
+              <p className="text-gray-600 mt-2 font-CG_Reg">Email: {friend.email}</p>
+              <p className="text-gray-600 mt-2 font-CG_Reg">Phone: {friend.phone}</p>
+            </li>
+          ))}
+        </ul>
+
+        <h2 className="text-2xl font-semibold mt-8 mb-4 font-reborn text-[#979D92]">Friend Requests</h2>
+        <ul>
+          {friendRequests.map((request) => (
+            <li key={request.username} className="mb-6">
+              <p className="text-lg font-medium font-CG_Reg">Name: {request.name}</p>
+              <p className="text-gray-600 mt-2 font-CG_Reg">Main Instrument: {request.main_instrument}</p>
+              <p className="text-gray-600 mt-2 font-CG_Reg">Bio: {request.bio}</p>
+              <p className="text-gray-600 mt-2 font-CG_Reg">Skills: {request.skills}</p>
+              <p className="text-gray-600 mt-2 font-CG_Reg">Accolades: {request.accolades}</p>
+              <div className="mt-4">
+                <button
+                  onClick={() => handleAcceptRequest(request)}
+                  className="bg-[#788073] text-white px-4 py-2 rounded-full mr-4 hover:bg-green-600"
+                >
+                  Accept
+                </button>
+                <button
+                  onClick={() => handleDeclineRequest(request)}
+                  className="bg-red-500 text-white px-4 py-2 rounded-full hover:bg-red-600"
+                >
+                  Decline
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
