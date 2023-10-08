882297689791681632b34e1fdb73deca96ab06fc
aekmekci72 <anna.ekmekci@gmail.com>
2023-10-08 11:48:14 -0400

    yay!

--- client/src/profilePictures/profilePictures.js
+++ client/src/profilePictures/profilePictures.js
@@ -4,6 +4,7 @@ const profilePictures = {1694386680124: require('./1694386680124.jpg'),
 1694663680142: require('./1694663680142.jpg'),
 1696713099196: require('./1696713099196.png'),
 1696778447726: require('./1696778447726.jpg'),
+1696780052986: require('./1696780052986.jpg'),
 default: require('./default.jpg'),
 pic: require('./pic.png'),}; 
 export default profilePictures;
